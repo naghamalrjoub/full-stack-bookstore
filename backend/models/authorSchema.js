e9548c61871986616aada56bc75c2566fe28dcc0
naghamalrjoub <nagham.rjoub@gmail.com>
2026-04-16 19:29:21 +0300

    author schema edits

--- backend/models/authorSchema.js
+++ backend/models/authorSchema.js
@@ -1,6 +1,15 @@
 const mongoose = require("mongoose")
 const authorSchema = new mongoose.Schema({
-    name: {type: String, required: true}
+    name: {type: String, required: true},
+    books: [{
+        book: {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: "Book"
+        }
+    }],
+    bio: {type: String, default: "no bio provided"},
+    birthdate: {type: Date, default: "no info provided"},
+    nationality: {type: String, default: "no info provided"}
 })
 
 module.exports = mongoose.model("Author", authorSchema)
