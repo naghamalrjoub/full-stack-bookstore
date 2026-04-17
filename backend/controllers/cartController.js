b7eea4d4410ecc9e254a01c0616228f5c4b595ff
naghamalrjoub <nagham.rjoub@gmail.com>
2026-04-17 23:09:39 +0300

    add book to cart

--- backend/controllers/cartController.js
+++ backend/controllers/cartController.js
@@ -1,4 +1,5 @@
 const cartModel = require("../models/cartSchema");
+const bookModel = require("../models/bookSchema");
 
 const getCart = async (req, res) => {
     const {id} = req.params;
@@ -19,4 +20,44 @@ const getCart = async (req, res) => {
     }
 }
 
-module.exports = {getCart}
+const addToCart = async (req, res) => {
+    const {book} = req.body;
+    const {id} = req.params;
+
+    try {
+        const cart = await cartModel.findOne({userId: id})
+        if (!cart) {
+            res.status(404).json("not found")
+        }
+        else {
+            const _book = await bookModel.findOne({_id: book})
+            const bookPrice = _book.price
+            let exists = cart.items.find(elem => {
+                return elem.book = book
+            })
+
+            if (exists) {
+                exists.quantity += 1;
+            }
+
+            else {
+                cart.items.push({
+                    book: book, 
+                    quantity: 1,
+                })
+            }
+            
+            cart.total += bookPrice;
+            const saved = await cart.save()
+            await saved.populate("items.book")
+            res.status(200).json(saved)
+        }
+    }
+
+    catch(err) {
+        res.status(500).json(err)
+    }
+
+}
+
+module.exports = {getCart, addToCart}
--- backend/routes/cartRouter.js
+++ backend/routes/cartRouter.js
@@ -1,7 +1,8 @@
 const express = require("express");
 const cartRouter = express.Router();
-const {getCart} = require("../controllers/cartController");
+const {getCart, addToCart} = require("../controllers/cartController");
 
 cartRouter.get("/:id", getCart);
+cartRouter.patch("/:id", addToCart); // 69e2894046481cc1bb052c33
 
 module.exports = cartRouter
