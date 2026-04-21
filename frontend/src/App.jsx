962122cfe5c6563ded9ff4e014df7d24a56bcc3d
naghamalrjoub <nagham.rjoub@gmail.com>
2026-04-21 23:14:31 +0300

    footer added

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,18 +4,21 @@ import { Routes, Route } from 'react-router-dom'
 import Home from './pages/Home'
 import Books from './pages/Books'
 import About from './pages/About'
+import Footer from './components/Footer'
 
 function App() {
   return (
-    <>
-        <Navbar></Navbar>
-
+    <div className='d-flex flex-column'>
+        <Navbar />
         <Routes>
             <Route path='/' element={<Home />}></Route>
             <Route path='/Books' element={<Books />}></Route>
             <Route path='/About' element={<About />}></Route>
         </Routes>
-    </>
+
+        <Footer />
+       
+    </div>
   )
 }
 
---
+++ frontend/src/components/Footer.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <div className='d-flex justify-content-center align-items-center' style={{backgroundColor: "rgb(85, 50, 40)", color: "white", height: "3rem"}}>
+        <p>book-store 2025, all rights reserved</p>
+    </div>
+  )
+}
+
+export default Footer
